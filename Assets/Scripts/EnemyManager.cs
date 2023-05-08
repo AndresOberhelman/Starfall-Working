7578474cf567c0a5190df2cdc76713595788f208
Andres Oberhelman <91444957+AndresOberhelman@users.noreply.github.com>
2023-05-08 17:53:50 -0500

    Level 1 maps and enemy counter changes

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -8,9 +8,19 @@ public class EnemyManager : MonoBehaviour
 {
     public static int enemiesDestroyed = 0;
     public TextMeshProUGUI enemiesDestroyedText;
+    public GameObject wall;
+    public int requiredKills = 19;
 
     void Update()
     {
-    enemiesDestroyedText.text = "Enemies Destroyed: " + enemiesDestroyed;
+        enemiesDestroyedText.text = "Enemies Destroyed: " + enemiesDestroyed;
+
+        // Check if the required number of kills has been reached
+        if (enemiesDestroyed >= requiredKills)
+        {
+            Debug.Log("Final Door Opened"); // Disable the wall GameObject
+            wall.SetActive(false);
+        }
     }
 }
+
